d03d166d87af9cebfe316779ed7906e23b483846
joaogilbertodev <143625822+joaogilbertodev@users.noreply.github.com>
2024-03-18 14:29:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
-var canvas= document.getElementById("myCanvas");
-var ctx= canvas.getContext("2d");
 var mouseEvent= "empty";
 var lastPositionX,lastPositionY;
+
+
+var canvas= document.getElementById("myCanvas");
+var ctx= canvas.getContext("2d");
+
+
 var color= "red";
 var wl= 1;
-
 canvas.addEventListener("mousedown", myMouseDown);
-function myMouseDown(e){
-    mouseEvent= "mouseDown"
+function myMouseDown(e)
+{
+     mouseEvent = "mouseDown";
 }
 
-
-
 canvas.addEventListener("mousemove", myMouseMove);
-function myMouseMove(e){
-    posMouseX= e.clientX-canvas.offsetLeft;
-    posMouseY= e.clientY-canvas.offsetTop;
+function myMouseMove(e)
+{
+    PositionMouseX = e.clientX - canvas.offsetLeft;
+    PositionMouseY = e.clientY - canvas.offsetTop;
 
-    if(mouseEvent=="mouseDown"){
-        ctx.beginPath();
-        ctx.strokeStyle=color;
-        ctx.lineWidth= wl;
-        ctx.moveTo(lastPositionX, lastPositionY);
-        ctx.lineTo(posMouseX, posMouseY);
-        ctx.stroke()
+    if (mouseEvent == "mouseDown") {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = wl;
+    ctx.moveTo(lastPositionX, lastPositionY);
+    ctx.lineTo(PositionMouseX, PositionMouseY);
+    ctx.stroke();
     }
-    lastPositionX=posMouseY;
-    lastPositionY=posMouseX;
+
+    lastPositionX = PositionMouseX; 
+    lastPositionY = PositionMouseY;
 }
 
 canvas.addEventListener("mouseup", myMouseUp);
-function myMouseUp(e){
-    mouseEvent= "mouseUp"
+function myMouseUp(e)
+{
+    mouseEvent = "mouseUP";
 }
 
 canvas.addEventListener("mouseleave", myMouseLeave);
-function myMouseLeave(e){
-    mouseEvent= "mouseleave"
+function myMouseLeave(e)
+{
+    mouseEvent = "mouseleave";
 }
